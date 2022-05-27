3f00f8f04863aa9bf5b691d5ed197c60131b31e1
scarleth <scarleth1306@gmail.com>
2022-05-26 23:03:39 -0400

    Delete pokedex directory

--- pokedex/src/main/java/com/modyo/pokedex/models/service/PokemonService.java
+++
@@ -1,9 +0,0 @@
-package com.modyo.pokedex.models.service;
-
-import com.modyo.pokedex.dto.PokemonResponse;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface PokemonService {
-    PokemonResponse getPokemon(String nameOrId) throws Exception;
-}
